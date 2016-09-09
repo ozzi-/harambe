3e3d0f08bb53113923c1c6b52f41be64de674e2f
ozzi- <oz@oz-web.com>
2016-09-09 16:16:37 +0200

    Update faq.php

--- view/faq.php
+++ view/faq.php
@@ -1,15 +1,18 @@
 <h2>FAQ</h2>
 <b>Do you have any rules?</b><br>
-<i>Don't be a dick. Do not ruin the game for anybody. That simple</i><br>
-<br>
+Don't be a dick. Do not ruin the game for anybody. That simple!<br>
+<br><br>
 <b>How does the application work?</b><br>
-<i>Get on Discord, play some games with us and we will see if we get along</i><br>
-<br>
+Get on Discord, play some games with us and we will see if we get along.<br>
+<br><br>
 <b>Is this a safe space?</b><br>
-<i>No, we like to swear, talk shit and are sometimes offensive in a playful way</i><br>
-<br>
+No, we like to swear, talk shit and are sometimes offensive in a playful way.<br>
+<br><br>
 <b>Where are you guys from?</b><br>
-<i>Germany, Norway, Switzerland, Canada, France, Italy, Great Britain, USA ... the list continues ... </i><br>
-<br>
+Germany, Norway, Switzerland, Canada, France, Italy, India, United Kingdom, USA ... the list continues ... <br>
+<br><br>
 <b>When do you usually play?</b><br>
-<i>17:00-22:00 UTC during the week. Pretty much throughout the whole weekends</i><br>
+17:00-22:00 UTC during the week. Pretty much throughout the whole weekends.<br>
+<br><br>
+<b>When was this clan founded?</b><br>
+On the 7th September 2016! Before that, we were part of the Insurgency Division of <a href="http://www.dedicatedrejects.com/">DedicatedRejects</a>.
